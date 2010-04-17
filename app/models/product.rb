75e5d0f49262e429224b205d6a95e98e16cc5169
yas <ilyukevich.victor@gmail.com>
2010-04-17 18:14:16 +0300

    закончен пункт 7.1 Создан контроллер store и вывод продуктов через него

--- app/models/product.rb
+++ app/models/product.rb
@@ -10,9 +10,13 @@ class Product < ActiveRecord::Base
 
   validates_format_of :image_url, :with => %r{\.(gif|jpg|png)$}i, :message => "Only gif, jpn and png allowed", :allow_blank => true
 
+  #Product.for_sale.all
+  named_scope :for_sale, :order => :title
+
 protected
   def price_must_be_at_least_cent
     errors.add(:price, "Must be more then zero") if price.nil? || price < 0.01
   end
+  
 end
 
