f4b7adad2417f3d1f4bebb3219fd039f798f78fc
yas <ilyukevich.victor@gmail.com>
2010-05-03 13:44:05 +0300

    сделал, чтобы принимались заказы и сохранялись в базе. без аякса. в англ книге страница 158

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -4,4 +4,12 @@ class LineItem < ActiveRecord::Base
   belongs_to :product
   #has_one
   #has_many
+
+  def self.from_cart_item(cart_item)
+    li = self.new
+    li.product = cart_item.product
+    li.quantity = cart_item.quantity
+    li.total_price = cart_item.price
+    li
+  end
 end
