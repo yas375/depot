543605b68b0616e7820eb81ca7f0148a5f3738c4
Victor <ilyukevich.victor@gmail.com>
2010-05-06 11:16:03 +0300

    добавил создание польователя в seed.db

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,4 @@
 PaymentType.create!(:name => 'Check')
 PaymentType.create!(:name => 'Credit card')
 PaymentType.create!(:name => 'Purchase order')
+User.create!(:name => 'admin', :password => 'admin', :password_confirmation => 'admin')
