234e256bf9345cf5a6876d6f6d06b6b87ed4d603
yas <ilyukevich.victor@gmail.com>
2010-05-14 20:12:11 +0300

    изменения, которые я делал по вводу интернационализации

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,8 @@ class ApplicationController < ActionController::Base
   before_filter :authorize, :except => :login
   helper :all # include all helpers, all the time
   protect_from_forgery  :secret => '8fc080370e56e929a2d5afca5540a0f7' # See ActionController::RequestForgeryProtection for details
-
+  before_filter :set_locale
+  
 
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
